bf7cbca1cf5de84b3b1a79522b63a2013d3a6617
Daisykitio <daisycheptoek6788@gmail.com>
2023-03-16 17:00:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (success)
+ * Return: Always0 (success)
  *
  */
 int main(void)
 {
 	write(2,
 	     "and that piece of arts is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+return (0);
 }
 
