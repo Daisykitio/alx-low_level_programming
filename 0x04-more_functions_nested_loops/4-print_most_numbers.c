5c26a3e42f4c77a4fc701eaf3059c4ab707874a7
Daisykitio <daisycheptoek6788@gmail.com>
2023-03-24 10:08:36 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
   *print _most_numbers - Print the numbers sinces 0 up to 9
@@ -8,18 +9,18 @@
 
 void print_most_numbers(void)
 {
-	int = 0;
-	for (; x <= 9; x++)
+int x = 0;
+for (; x <= 9; x++)
+{
+if (x == 2 || x == 4)
 	{
-		if (x == 2 || x == 4)
-		{
-		continue;
-		}
-		else
-		{
-		_putchar(x + '0');
-		}
+	continue;
 	}
-	_putchar('\n');
+	else
+	{
+	putchar(x + '0');
+	}
+}
+putchar('\n');
 }
 
